fac985e0bd2a08ef493d86006ac6a5cde47066a2
Abdulhadi5692HDI <87948364+Abdulhadi5692HDI@users.noreply.github.com>
2022-03-20 10:16:43 +0000

    Update

--- src/Main.php
+++ src/Main.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Abdulhadi5692HDI\SpamOnChat;
 
-use jojoe77777\FormAPI\CustomForm;
+
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\{Command, CommandSender};
-use pocketmine\utils\Config;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerChatEvent;
 
 class Main extends PluginBase implements Listener{
     public function onEnable() : void {
@@ -21,12 +19,7 @@ class Main extends PluginBase implements Listener{
            $this->getLogger()->notice("Warning: Console is being spammed");
         }
         
-        public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        switch($command->getName()){
-        case "require":
-             $sender->sendMessage("§aA lot of players!!!!!!");
-    }
-        }
+        
       
       
     
